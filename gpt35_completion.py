5acf7329f9112337bcf75ce7f0007a71088afcc4
kairosial <kairosial96.gmail.com>
2023-04-13 03:00:03 +0900

    230413_0300

---
+++ gpt35_completion.py
@@ -0,0 +1,20 @@
+import openai
+from config import CONFIG
+
+"""GPT3.5 completion"""
+
+def gpt35_completion(prompt_text):
+    openai.api_key = CONFIG['openai_api_key']
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompt_text}
+    ],
+    max_tokens=200,
+    temperature=0,
+    stop=['--', '\n', ';', '#'],
+    )
+    return completion.choices[0].message["content"]
+
+
+
